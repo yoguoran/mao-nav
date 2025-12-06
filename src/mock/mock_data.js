adcd7385cd07bf542999a25edd1a17e8121171d8
yoguoran <79782464+yoguoran@users.noreply.github.com>
2025-12-06 22:31:53 +0800

    chore: 更新导航数据 - 2025/12/6 22:31:52

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -89,6 +89,13 @@ export const mockData = {
           "url": "https://net.arsn.cn/",
           "description": "",
           "icon": "https://free.picui.cn/free/2025/08/14/689db70f59a3c.png"
+        },
+        {
+          "id": "site-1765031491093",
+          "name": "超好用的密码管理",
+          "url": "https://bitwarden.com/",
+          "description": "",
+          "icon": "https://bitwarden.com/favicon.ico"
         }
       ]
     },
