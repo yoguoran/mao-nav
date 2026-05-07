71efb7173408f1faa05c5f12a049e099cc9e5904
yoguoran <79782464+yoguoran@users.noreply.github.com>
2026-05-07 09:13:11 +0800

    chore: 更新导航数据 - 2026/5/7 09:13:09

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -9,7 +9,7 @@ export const mockData = {
         {
           "id": "site-1777970762565",
           "name": "股票监控",
-          "url": "http://dianqi.yoguoran.workers.dev",
+          "url": "http://dq.qinxiaobai.dpdns.org",
           "description": "",
           "icon": "https://free.picui.cn/free/2025/08/14/689db70f59a3c.png"
         },
